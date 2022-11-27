155dd6ed3e314244c686289996884e202dca9e0d
LfPi2 <lfpi2.contact@gmail.com>
2022-11-27 16:22:45 +0000

    Blocks updated

--- blocks.h
+++ blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon Command Update Interval Update Signal*/
-	{" ", "date +%0H:%0M", 1, 0},
-	{" ", "date +%A\\ %0d/%0m/%Y", 60, 0},
-	{" ", "acpi | grep -o -e '[0-9]*%'", 60, 0},
+	{"time: ", "date +%0H:%0M", 1, 0},
+	{"date: ", "date +%A\\ %0d/%0m/%Y", 60, 0},
+	{"battery: ", "acpi | grep -o -e '[0-9]*%'", 60, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
